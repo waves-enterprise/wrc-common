fcce426bbc96ab6ae326e6162829e2268e60a8ea
dgeorgiev <dgeorgiev@web3tech.ru>
2024-03-29 10:11:26 +0000

    [WTCH-220] Refactor for release

--- wrc20/wrc20-contract-app/build.gradle.kts
+++ wrc20/wrc20-contract-app/build.gradle.kts
@@ -1,23 +1,18 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-
-val vstCommonsVersion: String by project
-val mavenUser: String by project
-val mavenPassword: String by project
-val registry: String by rootProject.extra
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    id("org.springframework.boot")
-    id("com.wavesplatform.vst.contract-docker")
-    `maven-publish`
+    kotlin("jvm")
+    application
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 dependencies {
     implementation(project(":wrc10:wrc10-contract-app"))
     implementation(project(":wrc20:wrc20-contract-api"))
 
-    implementation("com.wavesplatform.we:vst-contract-grpc:$vstCommonsVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
     testImplementation("com.wavesenterprise:we-node-domain-test")
     testImplementation("com.wavesenterprise:we-contract-sdk-test")
@@ -26,59 +21,10 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-fun getDate(): String {
-    val current = LocalDateTime.now()
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")
-    return formatter.format(current)
-}
-
-val bootJar: BootJar by tasks
-bootJar.apply {
-    archiveClassifier.set("application")
-}
-
-val jar: Jar by tasks
-jar.apply {
-    enabled = true
-}
-
-tasks {
-    docker {
-        val imageVersion = if (version.toString().endsWith("-SNAPSHOT")) {
-            "$version-${getDate()}"
-        } else {
-            "$version"
-        }
-        name = "$registry/icore-sc/${project.name}:$imageVersion"
-        tags("latest")
-        files(bootJar.get().archiveFile)
-        noCache(true)
-        dependsOn(bootJar.get())
+tasks.withType<ShadowJar> {
+    manifest {
+        attributes["Main-Class"] = "com.wavesenterprise.wrc.wrc20.WRC20FTokenContractStarterKt"
     }
 }
 
-java {
-    withSourcesJar()
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "maven"
-            url = uri(
-                "https://artifacts.wavesenterprise.com/repository/" +
-                    if (project.version.toString().endsWith("-SNAPSHOT")) "maven-snapshots"
-                    else "maven-releases"
-            )
-            credentials {
-                username = mavenUser
-                password = mavenPassword
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-    }
-}
+project.setProperty("mainClassName", "com.wavesenterprise.wrc.wrc20.WRC20FTokenContractStarterKt")
